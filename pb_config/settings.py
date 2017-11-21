18d7d57c20524dda3eebf6aa4dfa4709dcfddb73
mmac.pers@gmail.com <michael@sixtymill.com>
2017-11-21 17:43:47 -0600

    added templates setting in settings.py

--- pb_config/settings.py
+++ pb_config/settings.py
@@ -97,7 +97,8 @@ ROOT_URLCONF = 'pb_config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(PROJECT_ROOT,'templates')],
+        #'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
